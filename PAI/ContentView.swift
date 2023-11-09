af14ff92015e8b1e00a037d4616130ac48cb794f
Naomi <farewellnay@gmail.com>
2023-11-09 20:13:47 +1300

    Initial Commit

---
+++ PAI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PAI
+//
+//  Created by Naomi Wu on 9/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, naomi!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PAI/PAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PAIApp.swift
+//  PAI
+//
+//  Created by Naomi Wu on 9/11/23.
+//
+
+import SwiftUI
+
+@main
+struct PAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
